98e4985e5aa4958ba5a41010b6b0997e70ae81d9
HariSatyam-914 <harisatyamchodishetti@gmail.com>
2025-03-19 18:36:30 +0530

    Login Logout Feature Done

---
+++ Vital_Sign/app/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    #path('login/', views.login_view,name='login'),
+    path('logout/', views.logout_view,name='logout'),
+    
+]
+
